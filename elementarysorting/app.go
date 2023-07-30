68a1a0e0163afaa39a974ce10a0e33941785ce11
Sh0He1666 <tkmr0606@gmail.com>
2023-07-30 13:53:05 +0900

    add sort algos

--- elementarysorting/app.go
+++ elementarysorting/app.go
@@ -1,6 +1,11 @@
 package elementarysorting
 
 // 挿入ソート
+// ・先頭の要素をソート済みとする
+// ・未ソートの部分がなくなるまで、以下の処理を繰り返す
+// 　・未ソートの先頭から要素を1つ取り出しvに記録する
+// 　・ソート済みの部分において、vより大きい要素を後方へ1つずつ移動する
+// 　・最後に空いた位置に「取り出した要素v」を挿入する
 func insertionSort(a []int) {
 	for c := 1; c < len(a); c++ {
 		evalTarget := a[c]
@@ -11,4 +16,39 @@ func insertionSort(a []int) {
 		}
 		a[i+1] = evalTarget
 	}
+}
+
+// バブルソート
+// 順番が逆になっている隣接要素がなくなるまで、次の処理を繰り返す
+// ・配列の末尾から隣接する要素を比べていき、大小関係が逆ならば交換する
+func bubbleSort(a []int) {
+	done := true
+	for done {
+		done = false
+		for i := len(a) - 1; i >= 1; i-- {
+			if a[i-1] > a[i] {
+				a[i], a[i-1] = a[i-1], a[i]
+				done = true
+			}
+		}
+	}
+}
+
+// 選択ソート
+// ・見ソートの部分から最小の要素の位置を特定する
+// ・最小の要素の位置にある要素と未ソートの部分の先頭要素を交換する
+func selectionSort(a []int) {
+	for c := 0; c < len(a); c++ {
+		minValueIndex := c
+		for i := c; i < len(a); i++ {
+			if a[i] < a[minValueIndex] {
+				minValueIndex = i
+			}
+		}
+		a[c], a[minValueIndex] = a[minValueIndex], a[c]
+	}
+}
+
+func stableSort(a []int) {
+
 }
--- elementarysorting/app_test.go
+++ elementarysorting/app_test.go
@@ -9,8 +9,24 @@ import (
 
 var testData []int
 
+func copyTestData() []int {
+	ret := make([]int, len(testData))
+	_ = copy(ret, testData)
+	return ret
+}
+
+func createRandomNumberArray(n int) []int {
+	a := make([]int, n)
+	i := 0
+	for i < n {
+		a[i] = rand.Intn(n)
+		i++
+	}
+	return a
+}
+
 func TestMain(t *testing.M) {
-	testData = createRandomNumberArray(10000)
+	testData = createRandomNumberArray(100000)
 	t.Run()
 }
 
@@ -46,17 +62,87 @@ func Test_insertionSort(t *testing.T) {
 }
 
 func Benchmark_insertionSort(b *testing.B) {
+	a := copyTestData()
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
-		insertionSort(testData)
+		insertionSort(a)
 	}
 }
 
-func createRandomNumberArray(n int) []int {
-	a := make([]int, n)
-	i := 0
-	for i < n {
-		a[i] = rand.Intn(n)
-		i++
+func Test_bubbleSort(t *testing.T) {
+	type in struct {
+		a []int
 	}
-	return a
+	cases := map[string]struct {
+		in   in
+		want []int
+	}{
+		"配列内の数字がユニーク": {
+			in: in{
+				[]int{5, 1, 3, 6, 8, 2, 4, 9, 7},
+			},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		"配列内に同じ値を含む": {
+			in: in{
+				[]int{5, 2, 3, 6, 8, 2, 3, 9, 7},
+			},
+			want: []int{2, 2, 3, 3, 5, 6, 7, 8, 9},
+		},
+	}
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			bubbleSort(tt.in.a)
+			if diff := cmp.Diff(tt.in.a, tt.want); diff != "" {
+				t.Errorf("bubbleSort() (-got +want):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Benchmark_bubbleSort(b *testing.B) {
+	a := copyTestData()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		bubbleSort(a)
+	}
+}
+
+func Test_selectionSort(t *testing.T) {
+	type in struct {
+		a []int
+	}
+	cases := map[string]struct {
+		in   in
+		want []int
+	}{
+		"配列内の数字がユニーク": {
+			in: in{
+				[]int{5, 1, 3, 6, 8, 2, 4, 9, 7},
+			},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		"配列内に同じ値を含む": {
+			in: in{
+				[]int{5, 2, 3, 6, 8, 2, 3, 9, 7},
+			},
+			want: []int{2, 2, 3, 3, 5, 6, 7, 8, 9},
+		},
+	}
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			selectionSort(tt.in.a)
+			if diff := cmp.Diff(tt.in.a, tt.want); diff != "" {
+				t.Errorf("selectionSort() (-got +want):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Benchmark_selectionSort(b *testing.B) {
+	a := copyTestData()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		selectionSort(a)
+	}
 }
